f9909f57bb93934c7913fb93a20df1977007a38e
SM. Iqbal Hossain <95683233+SM-IQBAL-HOSSAIN@users.noreply.github.com>
2022-02-15 12:59:20 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+
+for(var i= 0; i<3; i++){
+
+document.querySelectorAll(".myButton")[i].addEventListener("click", function (){
+
+var text = this.innerHTML; 
+    
+audioPlay(text); 
+    
+});
+
+}
+
+function audioPlay(text){
+    switch(text){
+        case "a":
+            var audio = new Audio('a.mp3');
+            audio.play(); 
+            break;
+                
+            case "b":
+                var audio = new Audio('b.mp3');
+                audio.play(); 
+                break;
+        
+                case "c":
+            var audio = new Audio('a.mp3');
+            audio.play(); 
+            break;
+               
+        
+        }
+
+
+}
